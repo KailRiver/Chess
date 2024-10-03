9d91c18b347c6563777a32d29ee48a3fbc6d861a
kailriver06@gmail.com <kailriver06@gmail.com>
2024-10-03 16:53:48 +0300

    made new feature about good job 2

--- src/feature.java
+++ src/feature.java
@@ -1,6 +1,7 @@
 
 public class feature {
     public static void main(String[] args) {
-        System.out.println("Good job");
+        String GJ = "Good job";
+        System.out.println(GJ);
     }
 }
